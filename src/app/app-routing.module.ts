b313e78e0ea0441ba705563fd50387197068caeb
slb <CSchellenberger@MuellerBBM-vas.de>
2024-09-11 11:04:59 +0200

    add flat routing to the new components (requires manual input in the browser address field: http://localhost:4200/intro)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { IntroComponent } from './intro/intro.component';
+import { CreationOperatorsComponent } from './creation-operators/creation-operators.component';
+
+const routes: Routes = [
+  { path: 'intro', component: IntroComponent },
+  { path: 'creation-operators', component: CreationOperatorsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
