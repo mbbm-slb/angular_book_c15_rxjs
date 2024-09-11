d410affb2c9ab8816c66169af53d6154f0095488
slb <CSchellenberger@MuellerBBM-vas.de>
2024-09-11 10:52:52 +0200

    add intro component (ng g component Intro)

---
+++ src/app/intro/intro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dr-intro',
+  templateUrl: './intro.component.html',
+  styleUrl: './intro.component.css'
+})
+export class IntroComponent {
+
+}
