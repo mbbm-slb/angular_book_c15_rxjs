e62d533695a79b565c41bdb9a6fd74ed9a5ad04c
slb <CSchellenberger@MuellerBBM-vas.de>
2024-09-11 20:43:35 +0200

    wip: higher order operators part 2

--- src/app/higher-order-operators/higher-order-operators.component.ts
+++ src/app/higher-order-operators/higher-order-operators.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { concatMap, from } from 'rxjs';
+import { concatMap, from, interval, Observable, Subscription, switchMap } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 @Component({
@@ -11,6 +11,7 @@ import { ajax } from 'rxjs/ajax';
 export class HigherOrderOperatorsComponent {
 
   start_3_1() {
+    console.clear();
     const urls = [
       'https://jsonplaceholder.typicode.com/todos',
       'https://jsonplaceholder.typicode.com/comments',
@@ -23,5 +24,56 @@ export class HigherOrderOperatorsComponent {
         })
       )
       .subscribe((response) => console.log(response.response));
+  }
+
+  start_3_2() {
+    console.clear();
+
+    // first observable that emits random intervals
+    const randomInterval$ = new Observable<number>((observer) => {
+      const emitValue = () => {
+        const delay = Math.floor(Math.random() * (10000 - 3000 + 1)) + 3000;
+        observer.next(delay / 1000); // Emit the interval in seconds
+        setTimeout(emitValue, delay);
+      };
+      emitValue();
+    });
+
+    // second observable that emits a value every 300ms and is restarted by the first observable
+    const switchInterval$ = randomInterval$.pipe(switchMap(() => interval(300))); 
+
+    this.subscribeWithTimeout<number>(
+      switchInterval$,
+      (value: number) => console.log('value:', value),
+      30000
+    );
+
+    // demonstrate the random interval observable
+    // this.subscribeWithTimeout<number>(
+    //   randomInterval$,
+    //   (value: number) => console.log('Random interval fired at:', value),
+    //   30000
+    // );
+  }
+
+  start_3_3() {
+
+  }
+  
+  // ================================================================================================
+  // Helper methods
+  // ================================================================================================
+
+  subscribeWithTimeout<T>(
+    observable: Observable<T>,
+    observer: any,
+    timeout: number
+  ): Subscription {
+    const subscription = observable.subscribe(observer);
+    setTimeout(() => {
+      subscription.unsubscribe();
+      console.log('Unsubscribed after timeout');
+    }, timeout);
+    return subscription;
   }
 }
