85449d229a52525bb856f51d801a034647a7f232
slb <CSchellenberger@MuellerBBM-vas.de>
2024-09-11 13:28:54 +0200

    add first simple Observable-Observer example

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { of } from 'rxjs';
+
 @Component({
   selector: 'dr-intro',
   templateUrl: './intro.component.html',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class IntroComponent {
 
+  start_1_1() {
+    const numbers$ = of(1,2,3)
+    numbers$.subscribe(v => console.log(v))
+    numbers$.subscribe(myObserver)
+    numbers$.subscribe(new MyObserver('obs1: '));
+  }
+}
+
+interface Observer {
+  next: (event: any) => void;
+  error: (error: any) => void;
+  complete: () => void;
+}
+
+const myObserver: Observer = {
+  next: (event: any) => console.log(event),
+  error: (error: any) => console.log(error),
+  complete: () => console.log('complete!')
+}
+
+class MyObserver {
+  constructor(private prefix: string) {}
+  next(event :any) { console.log(this.prefix + event)}
+  error(error :any) { console.log(this.prefix + error)}
+  complete() { console.log(this.prefix + 'complete!')}
 }
